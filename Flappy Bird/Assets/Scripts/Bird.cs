bc3b9dfafb0514e6b8b4ae91bc06697cbdf129fa
Kim Barcelo <kim.barcelo@nbs.systems>
2020-09-04 14:55:46 +0800

    Flappy Bird updated to Unity 2020.1.2f1 (64-bit)

---
+++ Flappy Bird/Assets/Scripts/Bird.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    private bool isDead = false; //Has the player collided with a wall?
+    Rigidbody2D rb2d; //Holds a reference to the Rigidbody2D component of the bird
+    Animator anim; //Reference to the Animator component.
+
+    public float upForce = 200.0f; //Upward force of the "flap".
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        rb2d = GetComponent<Rigidbody2D>();
+
+        //Get reference to the Animator component attached to this GameObject.
+        anim = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Don't allow control if the bird has died.
+        if (isDead == false)
+        {
+            //Look for input to trigger a "flap".
+            if (Input.GetMouseButtonDown(0))
+            {
+                //pag cinlick ng cinlick yung mouse, tatalong ng tatalong pabilis ng pabilis yung bird, parang nag bubuild up yung pagtalong ni bird
+                rb2d.velocity = Vector2.zero; //pataas at pababa lang naman si bird
+
+                //    new Vector2(rb2d.velocity.x, 0);
+                //..giving the bird some upward force.
+                rb2d.AddForce(new Vector2(0.0f, upForce));
+
+                //...tell the animator about it and then...
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If the bird collides with something set it to dead...
+        isDead = true;
+        rb2d.velocity = Vector2.zero;
+        //...tell the Animator about it...
+        anim.SetTrigger("Dead");
+        GameControl.instance.BirdDied();
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour
+{
+
+    public int columnPoolSize = 5;
+    public GameObject columnPrefab;
+    public float spawnRate = 4f; //How quickly columns spawn.
+
+
+    public float columnMin = -1f; //Minimum y value of the column position.
+    public float columnMax = 3.5f; //Maximum y value of the column position.
+    float spawnXPosition = 10f;
+    int currentColumn = 0; //Index of the current column in the collection.
+
+    Vector2 objectPoolPosition = new Vector2(-15f, -25f); //A holding position for our unused columns offscreen.
+    GameObject[] columns; //Collection of pooled columns.
+
+    float timeSinceLastSpawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialize the columns collection.
+        columns = new GameObject[columnPoolSize];
+        //Loop through the collection... 
+        for (int i = 0; i < columnPoolSize; i++)
+        {
+            //...and create the individual columns.
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    //This spawns columns as long as the game is not over.
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime; //counting up
+
+        if(GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate)
+        {
+            timeSinceLastSpawned = 0;
+
+            //Set a random y position for the column
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+
+            //...then set the current column to that position.
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            currentColumn++; //Increase the value of currentColumn. If the new size is too big, set it back to zero
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
+    }
+}
